6cead053de7e4e863677f4eb06c32842678fbef8
Oxters Wyzgowski <oxters168@gmail.com>
2022-03-14 21:04:47 -0400

    Added GPLv3 license

--- app/src/main/java/com/OxGames/OxShell/CustomFileProvider.java
+++ app/src/main/java/com/OxGames/OxShell/CustomFileProvider.java
@@ -1,18 +1,23 @@
 package com.OxGames.OxShell;
 
+import android.content.ContentProviderResult;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.MatrixCursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.CancellationSignal;
+import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
 import android.provider.DocumentsContract;
 import android.provider.DocumentsProvider;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.content.FileProvider;
 
 import java.io.File;
@@ -20,6 +25,8 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 
 public class CustomFileProvider extends DocumentsProvider {
+    private final String ROOT_FOLDER_ID = "roots";
+
     /* Default projection for a root when none supplied */
     private static final String[] DEFAULT_ROOT_PROJECTION = {
             DocumentsContract.Root.COLUMN_ROOT_ID,
@@ -30,6 +37,14 @@ public class CustomFileProvider extends DocumentsProvider {
             DocumentsContract.Root.COLUMN_SUMMARY,
             DocumentsContract.Root.COLUMN_DOCUMENT_ID,
 //            DocumentsContract.Root.COLUMN_AVAILABLE_BYTES
+    };
+    private static final String[] DEFAULT_DOCUMENT_PROJECTION = {
+            DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+            DocumentsContract.Document.COLUMN_MIME_TYPE,
+            DocumentsContract.Document.COLUMN_DISPLAY_NAME,
+            DocumentsContract.Document.COLUMN_LAST_MODIFIED,
+            DocumentsContract.Document.COLUMN_FLAGS,
+            DocumentsContract.Document.COLUMN_SIZE
     };
 
     public CustomFileProvider() {
@@ -70,11 +85,31 @@ public class CustomFileProvider extends DocumentsProvider {
 //        row.add(DocumentsContract.Root.COLUMN_FLAGS, DocumentsContract.Root.FLAG_SUPPORTS_EJECT);
 //        row.add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, "mno");
 //        return result;
+//        row.add(DocumentsContract.Root.COLUMN_ROOT_ID, ROOT_FOLDER_ID);
+//        row.add(DocumentsContract.Root.COLUMN_ICON, R.mipmap.ic_launcher);
+//        row.add(DocumentsContract.Root.COLUMN_TITLE, "OxShell");
+//        row.add(DocumentsContract.Root.COLUMN_FLAGS, 0);
+//        row.add(DocumentsContract.Root.COLUMN_DOCUMENT_ID, ROOT_FOLDER_ID);
+//        return result;
         return null;
     }
     @Override
-    public Cursor queryDocument(String s, String[] strings) throws FileNotFoundException {
-        Log.d("DocumentsProvider", "Query " + s + " " + (strings != null ? strings : "null"));
+    public Cursor queryDocument(String documentId, String[] projection) throws FileNotFoundException {
+        Log.d("DocumentsProvider", "Query " + documentId + " " + (projection != null ? projection : "null"));
+        //reference: https://github.com/jcraane/DocumentsProviderExample/blob/master/app/src/main/java/nl/jcraane/myapplication/provider/LocalDocumentsProvider.kt
+//        PagedActivity currentActivity = ActivityManager.GetCurrentActivity();
+//        final MatrixCursor result = new MatrixCursor(projection != null ? projection : DEFAULT_DOCUMENT_PROJECTION);
+//        if (documentId.equals(ROOT_FOLDER_ID)) {
+//            Log.d("DocumentsProvider", "Creating row for roots query");
+//            final MatrixCursor.RowBuilder row = result.newRow();
+//            row.add(DocumentsContract.Document.COLUMN_DOCUMENT_ID, ROOT_FOLDER_ID);
+//            row.add(DocumentsContract.Document.COLUMN_MIME_TYPE, DocumentsContract.Document.MIME_TYPE_DIR);
+//            row.add(DocumentsContract.Document.COLUMN_DISPLAY_NAME, currentActivity.getPackageName());
+//            row.add(DocumentsContract.Document.COLUMN_LAST_MODIFIED, null);
+//            row.add(DocumentsContract.Document.COLUMN_FLAGS, 0);
+//            row.add(DocumentsContract.Document.COLUMN_SIZE, null);
+//        }
+//        return result;
         return null;
     }
     @Override
@@ -85,21 +120,25 @@ public class CustomFileProvider extends DocumentsProvider {
     @Override
     public ParcelFileDescriptor openDocument(String s, String s1, @Nullable CancellationSignal cancellationSignal) throws FileNotFoundException {
         Log.d("DocumentsProvider", "Open " + s + " " + s1);
-        ParcelFileDescriptor data = null;
-        try {
-            data = ParcelFileDescriptor.open(new File(s), ParcelFileDescriptor.MODE_READ_ONLY);
-        } catch (FileNotFoundException ex) {
-            Log.d("FileChooser", ex.getMessage());
-        }
-        return data;
+//        ParcelFileDescriptor data = null;
+//        try {
+//            data = ParcelFileDescriptor.open(new File(s), ParcelFileDescriptor.MODE_READ_ONLY);
+//        } catch (FileNotFoundException ex) {
+//            Log.d("FileChooser", ex.getMessage());
+//        }
+//        return data;
+        return null;
     }
 
     public static Uri getUriForFile(String path) {
-//        final Uri data = FileProvider.getUriForFile(this, "com.OxGames.OxShell.Explorer", new File(path));
-//        grantUriPermission(getPackageName(), data, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//        final Intent intent = new Intent(Intent.ACTION_VIEW).setDataAndType(data, "video/*").addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         PagedActivity currentActivity = ActivityManager.GetCurrentActivity();
 //        Uri fileUri = FileProvider.getUriForFile(currentActivity, BuildConfig.DOCUMENTS_AUTHORITY, new File(path));
+//        ParcelFileDescriptor data = null;
+//        try {
+//            data = ParcelFileDescriptor.open(new File(path), ParcelFileDescriptor.MODE_READ_ONLY);
+//        } catch (FileNotFoundException ex) {
+//            Log.d("FileChooser", ex.getMessage());
+//        }
 
         Uri.Builder uriBuilder = new Uri.Builder();
         uriBuilder.scheme("content");
@@ -108,6 +147,8 @@ public class CustomFileProvider extends DocumentsProvider {
         Uri fileUri = uriBuilder.build();
         currentActivity.grantUriPermission(currentActivity.getPackageName(), fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION  | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
+//        ContentResolver resolver = currentActivity.getContentResolver();
+//        Cursor curses = resolver.query(Uri.parse(path), null, null, null);
 //        String documentId = DocumentsContract.getDocumentId(fileUri);
 //        Uri contractUri = DocumentsContract.buildDocumentUri(BuildConfig.DOCUMENTS_AUTHORITY, documentId);
         return fileUri;
